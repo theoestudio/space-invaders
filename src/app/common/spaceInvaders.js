37cb0948756f2a5d51a193adf005c62bf6a342d4
Nathaniel Inman <nate041010@gmail.com>
2016-12-11 15:07:16 -0600

    the timing and easing of the spaceships as they zigzag down is more definied.

--- src/app/common/spaceInvaders.js
+++ src/app/common/spaceInvaders.js
@@ -26,24 +26,32 @@ function zigZag(){
     invader.y = invader.startY = invader.originY = invader.endY;
     invader.endX = invader.terminalX = invader.terminalX - (v.w/6)|0;
     invader.tweenCurrent = 0;
-    invader.tweenDuration = 200;
-    invader.tweenType = 'linear';
+    invader.tweenDuration = 100;
+    invader.tweenType = 'ease-in-out-cubic';
     invader.state = 'left';
     invader.onEnd = function invaderFinished(invader){
       invader.x = invader.startX = invader.originX = invader.endX;
       invader.y = invader.startY = invader.originY = invader.endY;
       if(invader.state==='left'){
         invader.state = 'down-left';
-        invader.endY = invader.terminalY = invader.terminalY + 100;
+        invader.endY = invader.terminalY = invader.terminalY + invader.imageHeight;
+        invader.tweenDuration = 50;
+        invader.tweenType = 'ease-out-cubic';
       }else if(invader.state==='down-left'){
         invader.state = 'right';
         invader.endX = invader.terminalX = invader.terminalX + (v.w/6*2)|0;
+        invader.tweenDuration = 200;
+        invader.tweenType = 'ease-in-out-cubic';
       }else if(invader.state==='right'){
         invader.state = 'down-right';
-        invader.endY = invader.terminalY = invader.terminalY + 100;
+        invader.endY = invader.terminalY = invader.terminalY + invader.imageHeight;
+        invader.tweenDuration = 50;
+        invader.tweenType = 'ease-out-cubic';
       }else if(invader.state==='down-right'){
         invader.state = 'left';
         invader.endX = invader.terminalX = invader.terminalX - (v.w/6*2)|0;
+        invader.tweenDuration = 200;
+        invader.tweenType = 'ease-in-out-cubic';
       } //end if
       invader.tweenCurrent = 0;
     };
