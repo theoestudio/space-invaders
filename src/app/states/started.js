ea73466dc6d1521e4106511177bce9ffe53384a0
Nathaniel Inman <nate041010@gmail.com>
2016-12-22 16:56:39 -0600

    the player loses when the invader touches him/her.

--- src/app/states/started.js
+++ src/app/states/started.js
@@ -35,31 +35,47 @@ export function started(){
   // Detect invader collision with shields
   invaders.forEach((i,ii,io)=>{
     if(i.y+i.imageHeight>v.h-200){ //don't bother if not low enough
-      shields.forEach(s=>{ //shield
-        s.stacks.forEach((s,si,so)=>{ //stack
-          s.bricks.every((b,bi,bo)=>{ //brick
-            let l1 = i.x,              l2 = b.x,
-                r1 = i.x+i.imageWidth, r2 = b.x+b.width,
-                t1 = i.y,              t2 = b.y,
-                b1 = i.y+i.imageHeight,b2 = b.y+b.height,
-                result = false;
+      let p = player,
+          l1 = i.x,              l2 = p.x,
+          r1 = i.x+i.imageWidth, r2 = p.x+p.imageWidth,
+          t1 = i.y,              t2 = p.y,
+          b1 = i.y+i.imageHeight,b2 = p.y+p.imageHieght,
+          result = false;
 
-            // for collision, need to check all corners of one square
-            // to see if any of the corners are within the other
-            if(r1>=l2 && b1>=t2 && t1<=t2 && l1<=l2){
-              result = true;bo.splice(bi,1);
-            }else if(r1>=r2 && b1>=t2 && t1<=t2 && l1<=r2){
-              result = true;bo.splice(bi,1);
-            }else if(r1>=l2 && b1>=b2 && t1<=b2 && l1<=l2){
-              result = true;bo.splice(bi,1);
-            }else if(r1>=r2 && b1>=b2 && t1<=b2 && l1<=r2){
-              result = true;bo.splice(bi,1);
-            } //end if
-            if(s.bricks.length===0) so.splice(si,1); //remove empty stack
-            return true;
-          });
-        });
-      });
+      // for collision, need to check all corners of one square
+      // to see if any of the corners are within the other
+      if(r1>=l2 && b1>=t2 && t1<=t2 && l1<=l2){
+        result = true;scene.state = 'lost';
+      }else if(r1>=r2 && b1>=t2 && t1<=t2 && l1<=r2){
+        result = true;scene.state = 'lost';
+      }else if(r1>=l2 && b1>=b2 && t1<=b2 && l1<=l2){
+        result = true;scene.state = 'lost';
+      }else if(r1>=r2 && b1>=b2 && t1<=b2 && l1<=r2){
+        result = true;scene.state = 'lost';
+      } //end if
+      if(!result){
+        shields.forEach(s=>{ //shield
+          s.stacks.forEach((s,si,so)=>{ //stack
+            s.bricks.every((b,bi,bo)=>{ //brick
+              l2 = b.x;
+              r2 = b.x+b.width;
+              t2 = b.y;
+              b2 = b.y+b.height;
+              if(r1>=l2 && b1>=t2 && t1<=t2 && l1<=l2){
+                result = true;bo.splice(bi,1);
+              }else if(r1>=r2 && b1>=t2 && t1<=t2 && l1<=r2){
+                result = true;bo.splice(bi,1);
+              }else if(r1>=l2 && b1>=b2 && t1<=b2 && l1<=l2){
+                result = true;bo.splice(bi,1);
+              }else if(r1>=r2 && b1>=b2 && t1<=b2 && l1<=r2){
+                result = true;bo.splice(bi,1);
+              } //end if
+              if(s.bricks.length===0) so.splice(si,1); //remove empty stack
+              return true;
+            }); //end bricks
+          }); //end stacks
+        }); //end shields
+      } //end if
     } //end if
   });
 
