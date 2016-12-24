cdaed36cf08a0454ad102d0f6fd5387b7abfbdba
Nathaniel Inman <nate041010@gmail.com>
2016-12-23 21:00:14 -0600

    day cycle now maintains throughout the other states, introduced a point system. Things look much better.

--- src/app/states/initial.js
+++ src/app/states/initial.js
@@ -1,2 +1,26 @@
+import {shields} from '../shields';
+import {dayCycle} from '../background';
+
 export function initial(){
+  // Draw background
+  dayCycle.drawNext(true);
+
+  // Draw a ground
+  ctx.fillStyle='rgb(10,80,10)';
+  ctx.fillRect(0,v.h-10,v.w,10);
+
+  // Draw score
+  ctx.textAlign = 'left';
+  ctx.font = '24px Orbitron';
+  ctx.textBaseline = 'top';
+  ctx.fillStyle = '#000';
+  ctx.fillText('STARTING GAME...',15,15);
+
+  // Draw shields
+  ctx.fillStyle='#2dbfd4';
+  shields.forEach(shield=>{
+    shield.stacks.forEach(stack=>{
+      stack.bricks.forEach(b=> ctx.fillRect(b.x,b.y,b.width,b.height));
+    });
+  });
 } //end initial()
--- src/app/states/won.js
+++ src/app/states/won.js
@@ -1,7 +1,31 @@
+import {dayCycle} from '../background';
+
 export function won(){
+  // Draw background
+  dayCycle.drawNext(true);
+
+  // Draw a ground
+  ctx.fillStyle='rgb(10,80,10)';
+  ctx.fillRect(0,v.h-10,v.w,10);
+
+  // Draw main text
   ctx.fillStyle='#9F9';
   ctx.textAlign='center';
   ctx.textBaseline='middle';
   ctx.font = '48px Orbitron';
   ctx.fillText('WINNER!',v.w/2,v.h/2);
+
+  // Draw score
+  let bgColor = ctx.getImageData(15,15,1,1).data,
+      average = (bgColor[0]+bgColor[1]+bgColor[2])/3/255;
+
+  ctx.textAlign = 'left';
+  ctx.font = '24px Orbitron';
+  ctx.textBaseline = 'top';
+  if(average>0.5){
+    ctx.fillStyle='#000';
+  }else{
+    ctx.fillStyle='#fff';
+  } //end if
+  ctx.fillText(`SCORE: ${player.score}`,15,15);
 } //end won()
